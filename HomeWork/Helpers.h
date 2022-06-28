25833942a2cdc342c6efd6cc71fe552f6009cf51
Ian <80196814+Screll3@users.noreply.github.com>
2022-06-28 19:53:02 +0700

    h
    h

---
+++ HomeWork/Helpers.h
@@ -0,0 +1,4 @@
+long long PowSum(long long a, long long b)
+{
+	return (a + b) * (a + b);
+}
---
+++ HomeWork/HomeWork.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+#include "Helpers.h"
+
+int main()
+{
+    long long a, b;
+    std::cin >> a >> b;
+    std::cout << PowSum(a, b);
+}
